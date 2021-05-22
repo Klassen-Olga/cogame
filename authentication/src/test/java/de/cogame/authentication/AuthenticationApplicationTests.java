6f467ba8969b7f98aede073e1270de0f4f510881
Klassen-Olga <56605448+Klassen-Olga@users.noreply.github.com>
2021-05-22 14:59:43 +0200

    added authentication module

---
+++ authentication/src/test/java/de/cogame/authentication/AuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package de.cogame.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
