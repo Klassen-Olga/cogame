f3ab6826ca85d3b57b03fcace7fb43578eb7aae6
Klassen-Olga <56605448+Klassen-Olga@users.noreply.github.com>
2021-05-23 14:49:34 +0200

    implemented auth closes #6

--- authentication/src/main/java/de/cogame/authentication/web/AuthenticationController.java
+++ authentication/src/main/java/de/cogame/authentication/web/AuthenticationController.java
@@ -2,21 +2,21 @@ package de.cogame.authentication.web;
 
 import de.cogame.authentication.db.Authentication;
 import de.cogame.authentication.db.AuthenticationRepository;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Map;
-
+/*
+* Each request to cogame modules goes through this class
+* A request should have a "key" header with the appropriate value
+* Each service has its own authentication key
+* Traefik inserts an additional header Service-Name with the name of service
+* */
 @Log4j2
 @RestController
 public class AuthenticationController {
@@ -34,9 +34,8 @@ public class AuthenticationController {
     PasswordEncoder passwordEncoder;
 
     @GetMapping("/")
-    public void auth(@RequestHeader("X-Script-Name") String serviceName,
+    public void auth(@RequestHeader("Service-Name") String serviceName,
                             @RequestHeader("key") String key) {
-
 
         String serviceHash = getServiceHash(serviceName);
         log.info("KEY"+key);
@@ -45,8 +44,6 @@ public class AuthenticationController {
 
         }
     }
-
-
 
     private String getServiceHash(String serviceName) {
         List<Authentication> data = authenticationRepository.findByServiceName(serviceName);
