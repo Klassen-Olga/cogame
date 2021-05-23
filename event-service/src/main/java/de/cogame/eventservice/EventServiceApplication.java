5df09223b14189980076ec6ae2c93b670626ae57
Klassen-Olga <56605448+Klassen-Olga@users.noreply.github.com>
2021-05-23 17:35:37 +0200

    Revert "moved security to user service from global handler"
    This reverts commit 726ba722190afe876508be1eb77c6a0206d4a38f.

--- event-service/src/main/java/de/cogame/eventservice/EventServiceApplication.java
+++ event-service/src/main/java/de/cogame/eventservice/EventServiceApplication.java
@@ -1,6 +1,7 @@
 package de.cogame.eventservice;
 
 import de.cogame.globalhandler.exception.CustomExceptionHandler;
+import de.cogame.globalhandler.security.SecurityConfiguration;
 import de.cogame.globalhandler.swagger.SwaggerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,7 +9,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Import;
 
 @EnableFeignClients
-@Import({CustomExceptionHandler.class, SwaggerConfig.class})
+@Import({CustomExceptionHandler.class, SecurityConfiguration.class, SwaggerConfig.class})
 @SpringBootApplication
 public class EventServiceApplication {
 
--- message-service/src/main/java/de/cogame/messageservice/MessageServiceApplication.java
+++ message-service/src/main/java/de/cogame/messageservice/MessageServiceApplication.java
@@ -1,13 +1,14 @@
 package de.cogame.messageservice;
 
 import de.cogame.globalhandler.exception.CustomExceptionHandler;
+import de.cogame.globalhandler.security.SecurityConfiguration;
 import de.cogame.globalhandler.swagger.SwaggerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import({CustomExceptionHandler.class, SwaggerConfig.class})
+@Import({CustomExceptionHandler.class, SecurityConfiguration.class, SwaggerConfig.class})
 public class MessageServiceApplication {
 
     public static void main(String[] args) {
